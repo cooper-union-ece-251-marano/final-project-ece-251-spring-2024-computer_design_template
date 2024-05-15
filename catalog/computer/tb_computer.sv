e1398e1914fdf5cd05ceb1dcf47edab252b39ca3
temp <temp@temps-MacBook-Pro.local>
2024-05-14 20:07:04 -0400

    feat: R, I, J timing diagram

--- catalog/computer/tb_computer.sv
+++ catalog/computer/tb_computer.sv
@@ -42,6 +42,8 @@ module tb_computer;
     secondTest = 1'b0;
     $dumpfile("tb_computer.vcd");
     $dumpvars(0,dut,dut1,clk,reset,writedata,dataadr,memwrite);
+    //$dumpvars(clk, dut,  dut.instr); For R, I, J Timing diagram creation
+
     $monitor("t=%t\t0x%7h\t%7d\t%8d",$realtime,writedata,dataadr,memwrite);
     // $dumpvars(0,clk,a,b,ctrl,result,zero,negative,carryOut,overflow);
     // $display("Ctl Z  N  O  C  A                    B                    ALUresult");
